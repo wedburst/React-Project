dac60bc573322b79b09570d2faf718e7fdb79d7d
Jhon Abou <jhon.abou@teayudo.pe>
2021-01-05 17:44:02 -0500

    add install and more

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/sass/main.sass";
+import "./assets/styles/styles.css";
+import Logo from "./assets/images/react-icons.svg";
 
 function App() {
   return (
     <div className="container">
-      <h1 className="display-5">Paqari Tienda!</h1>
+      <div className="react-model">
+        <div>
+          <img src={Logo} className="iconos" alt="" />
+          <p className="color">Bienvenido a tu proyecto de ReactJs</p>
+        </div>
+      </div>
     </div>
   );
 }
